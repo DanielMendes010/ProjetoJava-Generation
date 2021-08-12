dcb9c7eea36a908fe5c1db58f7bbae9ee283e405
DanielMendes010 <danielinacio101@gmail.com>
2021-08-12 19:57:37 -0300

    Teste Vocacional

---
+++ src/ProjetoJava/Testando.java
@@ -0,0 +1,12 @@
+package ProjetoJava;
+
+public class Testando {
+
+	public static void main(String[] args) {
+		teste_vocacional_metodo inteligencia = new teste_vocacional_metodo();
+		
+		inteligencia.inteligencias();
+	}
+
+}
+
